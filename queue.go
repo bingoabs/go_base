4b0006a7a36d0348279bf8d265f66ea7b91b24f3
Bingo <bingoabs@gmail.com>
2022-02-17 16:56:13 +0800

    Init

---
+++ queue.go
@@ -0,0 +1,21 @@
+package go_base
+
+type Queue []interface{}
+
+func (q *Queue) Push(x interface{}) {
+	*q = append(*q, x)
+}
+
+func (q *Queue) Pop() interface{} {
+	h := *q
+	var el interface{}
+	l := len(h)
+	el, *q = h[0], h[1:l]
+	// Or use this instead for a Stack
+	// el, *q = h[l-1], h[0:l-1]
+	return el
+}
+
+func NewQueue() *Queue {
+	return &Queue{}
+}
